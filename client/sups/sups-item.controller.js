efe6108a419dd6b94fea9c5da45b427beee4497f
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-12 15:03:52 -0400

    sup html and js files.

---
+++ client/sups/sups-item.controller.js
@@ -0,0 +1,6 @@
+
+function SupsItemController() {
+
+}
+
+export default SupsItemController;
---
+++ client/sups/sups-page.controller.js
@@ -0,0 +1,12 @@
+
+function SupsPageController() {
+	const ctrl = this;
+
+	ctrl.sup = {
+		text: 'Hey from sups page',
+		created_date: new Date(Date.now()),
+	};
+
+}
+
+export default SupsPageController;
