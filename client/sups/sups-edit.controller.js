c5ea5128beac040cd76ac35fd67f6f8d004002b6
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-13 14:09:58 -0400

    Flash new sup message and disappear.

--- client/sups/sups-edit.controller.js
+++ client/sups/sups-edit.controller.js
@@ -1,7 +1,13 @@
+import { merge } from 'ramda';
+
 
 function SupsEditController() {
 	const ctrl = this;
 	ctrl.editedSup = {};
+
+	ctrl.$onChanges = function $onChanges() {
+		ctrl.editedSup = merge({}, ctrl.sup);
+	};
 
 	ctrl.saveSup = function saveSup() {
 		ctrl.save({ editedSup: ctrl.editedSup });
--- client/sups/sups-page.controller.js
+++ client/sups/sups-page.controller.js
@@ -1,6 +1,7 @@
 
-function SupsPageController(supsAPIService, $interval) {
+function SupsPageController(supsAPIService, flashesService, $interval) {
 	const ctrl = this;
+	ctrl.editedSup = {};
 
 	function getSups() {
 		supsAPIService.sups.get().$promise.then((data)=> {
@@ -17,6 +18,8 @@ function SupsPageController(supsAPIService, $interval) {
 				savedSup,
 					ctrl.sups,
 			];
+			ctrl.editedSup = {};
+			flashesService.displayMessage('Sup Created!', 'success');
 		});
 	};
 }
